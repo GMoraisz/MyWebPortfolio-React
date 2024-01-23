437f69c04de01a4ea64fe944ba0d048b5fdb3642
Gabriel Morais <moraisgabriel923@gmail.com>
2024-01-23 00:52:05 -0300

    save

--- src/Components/Pages/Contact/Contact.js
+++ src/Components/Pages/Contact/Contact.js
@@ -1,31 +1,53 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useState, useRef } from 'react';
 import styles from './Contact.module.css';
-import { PiArrowSquareDownFill } from "react-icons/pi";
+import { PiArrowSquareDownFill } from 'react-icons/pi';
+import emailjs from '@emailjs/browser';
 
-export const Contact = () => {
-    const form = useRef();
+const Contact = () => {
+    const form = useRef(); 
+    
 
-    const sendEmail = (e) => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('YOUR_SERVICE_ID', 'template_uens4n3', form.current, 'AOcM8JDhqP7Q83bT2')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
+        const serviceId = 'service_ds8wwfi';
+        const templateId = 'template_q6pp0bo';
+        const publicKey = 'AOcM8JDhqP7Q83bT2';
+
+        const templateParams = {
+            from_name: name,
+            from_email: email,
+            to_name: 'Gabriel Morais',
+            message: message,
+        };
+
+        emailjs
+            .send(serviceId, templateId, templateParams, publicKey)
+            .then((response) => {
+                console.log('Email enviado com sucesso!', response);
+                setName('');
+                setEmail('');
+                setMessage('');
+            })
+            .catch((error) => {
+                console.error('Erro ao enviar o e-mail:', error);
             });
     };
 
     return (
         <div className={styles.formContact}>
             <h2 className={styles.formTitle}>
-                Entre em Contato <PiArrowSquareDownFill/> </h2>
-            <form ref={form} onSubmit={sendEmail}>
+                Entre em Contato <PiArrowSquareDownFill />{' '}
+            </h2>
+            <form ref={form} onSubmit={handleSubmit}> 
                 <label>Nome: </label>
-                <input type="text" name="from_name" />
-                <label>E-mail:  </label>
-                <input type="email" name="from_email" />
+                <input type="text" name="name" />
+                <label>E-mail: </label>
+                <input type="email" name="email" />
                 <label>Mensagem: </label>
                 <textarea name="message" />
                 <input type="submit" value="Enviar" />
@@ -34,4 +56,4 @@ export const Contact = () => {
     );
 };
 
-export default Contact;
+export default Contact; 
